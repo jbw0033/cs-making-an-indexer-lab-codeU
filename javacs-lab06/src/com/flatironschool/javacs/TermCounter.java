aabcba90fa0c11b93fd3d0fc976a588b91e2dce7
jbw0033 <jbw0033@auburn.edu>
2016-05-13 02:14:36 -0500

    final commit for indexer

--- javacs-lab06/src/com/flatironschool/javacs/TermCounter.java
+++ javacs-lab06/src/com/flatironschool/javacs/TermCounter.java
@@ -36,8 +36,11 @@ public class TermCounter {
 	 * @return
 	 */
 	public int size() {
-        // TODO: fill this in.
-		return -1;
+        int size = 0;
+        for(String term: keySet()) {
+        	size += get(term);
+        }
+		return size;
 	}
 
 	/**
